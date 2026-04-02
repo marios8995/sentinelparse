1572ac8b8defd42fde8aea7eff85aec3f0a40e4e
marios8995 <samoila.mario@proton.me>
2026-04-02 19:00:25 +0300

    Prepared webpage to allow switching between latest data and historical data.

---
+++ dashboard/src/pages/DashboardHistorical.jsx
@@ -0,0 +1,11 @@
+export default function DashboardHistorical () {
+  return (
+    <div className="p-4 bg-ctp-light-mantle dark:bg-ctp-dark-mantle rounded-xl border border-ctp-light-surface0 dark:border-ctp-dark-surface0">
+      <h2 className="text-2xl font-bold mb-4 text-ctp-light-blue dark:text-ctp-dark-blue">System History</h2>
+      <p className="italic opacity-70">
+        Aggregate data visualization coming soon. This view will display Hourly, Daily, and Monthly trends.
+      </p>
+      
+    </div>
+  )
+}
---
+++ dashboard/src/pages/DashboardLatest.jsx
@@ -0,0 +1,48 @@
+import { useState, useEffect } from 'react'
+import CpuComp from '../components/CpuComp'
+import RamComp from '../components/RamComp'
+import NetComp from '../components/NetComp'
+import SysComp from '../components/SysComp'
+import DiskComp from '../components/DiskComp'
+
+const getApiUrl = () => {
+  const hostname = window.location.hostname;
+  return `http://${hostname}:8000`
+}
+
+export default function DashboardLatest() {
+  const [hardwareData, setHardwareData] = useState(null)
+  const [error, setError] = useState(null)
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(getApiUrl() + '/api/latest/raw')
+        if (!response.ok) throw new Error("Network response was not ok")
+        const data = await response.json()
+        setHardwareData(data)
+      } catch (err) {
+        setError(err.message)
+      }
+    }
+
+    fetchData()
+    const intervalId = setInterval(fetchData, 2000)
+    return () => clearInterval(intervalId)
+  }, [])
+
+  if (error) return <p className="text-red-500">API Error: {error}</p>
+  if (!hardwareData) return <p>Connecting to probe...</p>
+
+  return (
+    <div className="space-y-4">
+      <CpuComp data={hardwareData} />
+      <div className="flex flex-wrap lg:flex-nowrap gap-4 justify-center">
+        <RamComp data={hardwareData} />
+        <NetComp data={hardwareData} />
+        <SysComp data={hardwareData} />
+      </div>
+      <DiskComp data={hardwareData} />
+    </div>
+  )
+}
